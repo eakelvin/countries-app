e8f5abd67195b870fbce603add09e0af308b6c22
Kelvin Akaba <akabakelvin@gmail.com>
2023-10-12 18:55:26 +0000

    search form commit

---
+++ src/Components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchForm from './Search'
+
+const Body = () => {
+  return (
+    <div>
+      <SearchForm />
+    </div>
+  )
+}
+
+export default Body
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { BsMoon } from "react-icons/bs";
 
-function Navbar() {
+function Header() {
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <h2 className='font-medium'>Where in the World?</h2>
-        <div className="">
-          <button
-            type="button"
-            className="flex md:order-2 text-dark  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  text-sm px-4 py-2 text-center mr-3 md:mr-0  dark:focus:ring-blue-800"
-          >
-            <BsMoon />
-            <span className='pl-2'>Dark Mode</span>
-          </button>
-        </div>
+    <header className="lg:px-16 px-4 bg-white flex flex-wrap items-center py-4 shadow-md">
+      <div className="flex-1 flex justify-between items-center">
+        <span className="text-xl font-bold">Where in the World?</span>
       </div>
-    </nav>
+
+      <div className="flex">
+        <nav>
+          <div className="md:flex items-center justify-between text-base text-gray-700">
+            <button
+              type="button"
+              className="flex md:order-2 text-dark  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  text-sm px-4 py-2 text-center mr-3 md:mr-0  dark:focus:ring-blue-800"
+            >
+              <BsMoon />
+              <span className='pl-2'>Dark Mode</span>
+            </button>
+          </div>
+        </nav>
+      </div>
+    </header>
   );
 }
 
-export default Navbar;
+export default Header;
