9e46db25cf501be6a5289acb64c8bc6eacfed8ee
Kelvin Akaba <akabakelvin@gmail.com>
2023-10-17 22:40:20 +0000

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom'
 import Home from './Pages/Home'
